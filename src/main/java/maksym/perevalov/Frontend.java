6520a6a92095c8b300880da9b3be15925b3cb692
maksym.perevalov <maksym.perevalov@sigma.software>
2023-10-10 23:12:13 +0300

    step 2

--- src/main/java/maksym/perevalov/Frontend.java
+++ src/main/java/maksym/perevalov/Frontend.java
@@ -1,2 +1,186 @@
-package maksym.perevalov;public class Frontend {
+package maksym.perevalov;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Frontend {
+    sealed interface Token {
+
+        int index();
+
+        record StartExpression(int index) implements Frontend.Token {
+        }
+
+        record EndExpression(int index) implements Frontend.Token {
+        }
+
+        record OpenBracket(int index) implements Frontend.Token {
+        }
+
+        record ClosedBracket(int index) implements Frontend.Token {
+        }
+
+        record Comma(int index) implements Frontend.Token {
+        }
+
+        record Operation(String value, int index) implements Frontend.Token {
+        }
+
+        record Number(String value, int index) implements Frontend.Token {
+        }
+
+        record Identifier(String value, int index) implements Frontend.Token {
+        }
+    }
+
+    // a + sin ( 60 )
+    public static void main(String[] args) {
+        String test = "(*a+b)";
+        var parser = new Parser();
+        var tokens = parser.parse(test);
+        System.out.println("---TOKENS---");
+        tokens.forEach(System.out::println);
+        var analyzer = new SyntaxAnalyzer();
+        var errors = analyzer.validate(tokens);
+        System.out.println("---Errors---");
+        if (errors.isEmpty()) {
+            System.out.println("No errors :)");
+        }
+        errors.forEach(System.out::println);
+    }
+
+    static class SyntaxAnalyzer {
+        List<String> validate(List<Token> tokens) {
+            Token state = tokens.get(0);
+            var errors = new ArrayList<String>();
+            for (int i = 1; i < tokens.size(); i++) {
+                var token = tokens.get(i);
+                switch (state) {
+                    case Token.StartExpression _ -> {
+                        if (token instanceof Token.Number || token instanceof Token.Identifier || token instanceof Token.OpenBracket) {
+                            break;
+                        }
+                        if (token instanceof Token.Operation op) {
+                            if (Objects.equals(op.value, "-") || Objects.equals(op.value, "+")) {
+                                break;
+                            }
+                        }
+                        errors.add("Invalid token '%s' after '%s' at index '%s'".formatted(token.getClass().getSimpleName(), state.getClass().getSimpleName(), token.index()));
+                    }
+                    case Token.OpenBracket _ -> {
+                        if (token instanceof Token.Number || token instanceof Token.Identifier) {
+                            break;
+                        }
+                        if (token instanceof Token.Operation op) {
+                            if (Objects.equals(op.value, "-") || Objects.equals(op.value, "+")) {
+                                break;
+                            }
+                        }
+                        errors.add("Invalid token '%s' after '%s' at index '%s'".formatted(token.getClass().getSimpleName(), state.getClass().getSimpleName(), token.index()));
+                    }
+                    case Token.ClosedBracket _ -> {
+                        if (token instanceof Token.EndExpression) {
+                            break;
+                        }
+                        if (token instanceof Token.Operation op) {
+                            if (List.of("+", "-", "*", "/").contains(op.value())) {
+                                break;
+                            }
+                        }
+                        errors.add("Invalid token '%s' after '%s' at index '%s'".formatted(token.getClass().getSimpleName(), state.getClass().getSimpleName(), token.index()));
+                    }
+                    case Token.Number _ -> {
+                        if (token instanceof Token.Operation || token instanceof Token.ClosedBracket || token instanceof Token.EndExpression) {
+                            break;
+                        }
+                        errors.add("Invalid token '%s' after '%s' at index '%s'".formatted(token.getClass().getSimpleName(), state.getClass().getSimpleName(), token.index()));
+                    }
+                    case Token.Identifier _ -> {
+                        if (token instanceof Token.Operation || token instanceof Token.ClosedBracket || token instanceof Token.EndExpression) {
+                            break;
+                        }
+                        errors.add("Invalid token '%s' after '%s' at index '%s'".formatted(token.getClass().getSimpleName(), state.getClass().getSimpleName(), token.index()));
+                    }
+                    case Token.Operation _ -> {
+                        if (token instanceof Token.Number || token instanceof Token.Identifier || token instanceof Token.OpenBracket) {
+                            break;
+                        }
+                        errors.add("Invalid token '%s' after '%s' at index '%s'".formatted(token.getClass().getSimpleName(), state.getClass().getSimpleName(), token.index()));
+                    }
+                    case Token.EndExpression _, Token.Comma _ -> {}
+                }
+                state = token;
+            }
+            return errors;
+        }
+    }
+
+    static class Parser {
+        private static final List<Character> OPERATION = List.of('-', '+', '*', '/', '^');
+
+        List<Token> parse(String text) {
+            var chars = text.replaceAll(" ", "").toCharArray();
+            var tokens = new ArrayList<Token>(List.of(new Token.StartExpression(-1)));
+            int i = 0;
+            while (i != chars.length) {
+                char currentChar = chars[i];
+                if (currentChar == '(') {
+                    tokens.add(new Token.OpenBracket(i));
+                    i++;
+                    continue;
+                }
+                if (currentChar == ')') {
+                    tokens.add(new Token.ClosedBracket(i));
+                    i++;
+                    continue;
+                }
+                if (currentChar == ',') {
+                    tokens.add(new Token.Comma(i));
+                    i++;
+                    continue;
+                }
+                if (OPERATION.contains(currentChar)) {
+                    tokens.add(new Token.Operation(String.valueOf(currentChar), i));
+                    i++;
+                    continue;
+                }
+                if (Character.isDigit(currentChar)) {
+                    String number = String.valueOf(currentChar);
+                    int startIndex = i;
+                    while (i != chars.length - 1) {
+                        currentChar = chars[++i];
+                        if (Character.isDigit(currentChar)) {
+                            number += String.valueOf(currentChar);
+                        } else {
+                            --i;
+                            break;
+                        }
+                    }
+                    i++;
+                    tokens.add(new Token.Number(number, startIndex));
+                    continue;
+                }
+                if (Character.isAlphabetic(currentChar)) {
+                    String identifier = String.valueOf(currentChar);
+                    int startIndex = i;
+                    while (i != chars.length - 1) {
+                        currentChar = chars[++i];
+                        if (Character.isAlphabetic(currentChar)) {
+                            identifier += String.valueOf(currentChar);
+                        } else {
+                            --i;
+                            break;
+                        }
+                    }
+                    i++;
+                    tokens.add(new Token.Identifier(identifier, startIndex));
+                    continue;
+                }
+                throw new RuntimeException("Unexpected symbol '%s' at index '%s'".formatted(currentChar, i));
+            }
+            tokens.add(new Token.EndExpression(chars.length));
+            return tokens;
+        }
+    }
 }
--- src/main/java/maksym/perevalov/SimpleCompiler.java
+++ src/main/java/maksym/perevalov/SimpleCompiler.java
@@ -2,112 +2,27 @@ package maksym.perevalov;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-@Command(name = "checksum")
-public class Main {
+@Command(name = "run")
+public class SimpleCompiler implements Callable<Integer> {
     private static final List<Character> OPERATIONS = List.of('-', '+', '*', '/');
 
-    // number, operation, start, end, id
+    @Option(names = {"-e", "--expression"})
+    private String expression = "SHA-256";
+
+    @Override
+    public Integer call()  {
+        System.out.println("Your expression - "  + expression);
+        return 0;
+    }
+
     public static void main(String[] args) {
-        var expression = "(a+b)/cos(30)";
-
-        var tokenizer = new Tokenizer(expression.toCharArray());
-        var tokens = tokenizer.readAll();
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
-    }
-
-    sealed interface Token {
-        record Start() implements Token {}
-        record End() implements Token {}
-        record BeginBracket() implements Token {}
-        record EndBracket() implements Token {}
-        record Operation(String value) implements Token {}
-        record Number(String value) implements Token {}
-        record Id(String value) implements Token {}
-    }
-
-    static class Tokenizer {
-        private final char[] content;
-        private final int lastIndex;
-        private int currentIndex;
-
-        public Tokenizer(char[] input) {
-            content = input;
-            lastIndex = input.length -1;
-            currentIndex = -1;
-        }
-
-        public Token nextToken() {
-            if (currentIndex == -1) {
-                currentIndex++;
-                return new Token.Start();
-            }
-            if (currentIndex > lastIndex) {
-                return new Token.End();
-            }
-
-            Character character = next();
-
-            return switch (character) {
-                case '(' -> new Token.BeginBracket();
-                case ')' -> new Token.EndBracket();
-                case Character c when OPERATIONS.contains(c) -> new Token.Operation(c.toString());
-                case Character c when Character.isDigit(c) -> nextNumber(c);
-                case Character c when Character.isAlphabetic(c) -> nextId(c);
-                default -> throw new RuntimeException("Illegal token");
-            };
-        }
-
-        public List<Token> readAll() {
-            var tokens = new ArrayList<Token>();
-            var token = nextToken();
-            while (!(token instanceof Token.End)) {
-                tokens.add(token);
-                token = nextToken();
-            }
-            tokens.add(new Token.End());
-            return tokens;
-        }
-
-        private Token.Number nextNumber(Character firstDigit) {
-            String number = String.valueOf(firstDigit);
-            while (hasNext()) {
-                var next = next();
-                if (Character.isDigit(next)) {
-                    number = number + next;
-                } else {
-                    currentIndex--;
-                    return new Token.Number(number);
-                }
-            }
-            return new Token.Number(number);
-        }
-
-        private Token.Id nextId(Character firstDigit) {
-            String number = String.valueOf(firstDigit);
-            while (hasNext()) {
-                var next = next();
-                if (Character.isAlphabetic(next)) {
-                    number = number + next;
-                } else {
-                    currentIndex--;
-                    return new Token.Id(number);
-                }
-            }
-            return new Token.Id(number);
-        }
-
-        private char next() {
-            return content[currentIndex++];
-        }
-
-
-        private boolean hasNext() {
-            return currentIndex <= lastIndex;
-        }
+        int exitCode = new CommandLine(new SimpleCompiler()).execute(args);
+        System.exit(exitCode);
     }
 }
